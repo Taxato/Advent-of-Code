6010fb522a855d280af5d937a23ac4410b55327b
Taxato <thomag1004@hotmail.no>
2023-10-23 22:56:38 +0200

    Update day7.js

--- 2015/day7.js
+++ 2015/day7.js
@@ -5,7 +5,12 @@ function processInput(err, raw) {
 	if (err) return err;
 	const input = raw.replaceAll("\r", "").split("\n");
 
+	const regex =
+		/(?<in1>[a-z0-9]+)? ?(?<op>[A-Z]+)? ?(?<in2>[a-z0-9]+)? -> (?<out>\w+)/;
+
+	console.log(input[0].match(regex));
+
 	const output = input;
 
-	console.log(output);
+	// console.log(output);
 }
