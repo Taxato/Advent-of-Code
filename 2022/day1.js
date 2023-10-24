390a0340a0f409327eefa0d239b42d3eb1b471b4
Taxato <taxatoftw@gmail.com>
2023-10-24 10:14:08 +0200

    2022 day1

--- 2022/day1.js
+++ 2022/day1.js
@@ -1,17 +1,23 @@
-import { day1 as input } from "./inputs.json" assert {type:"json"}
+import { readFile } from "node:fs";
+readFile("./day1input.txt", { encoding: "ascii" }, processInput);
 
 const regex = /((?:\d+\n)+)/gm;
 
-const output = input
-	.match(regex)
-	.map(elf =>
-		elf
-			.split("\n")
-			.map(n => +n)
-			.reduce((sum, cur) => (sum += cur), 0)
-	)
-	.sort((a, b) => b - a)
-	.slice(0, 3)
-	.reduce((sum, cur) => (sum += cur), 0);
+function processInput(err, raw) {
+	if (err) return err;
+	const input = raw.replaceAll("\r", "");
 
-console.log(output);
+	const output = input
+		.match(regex)
+		.map(elf =>
+			elf
+				.split("\n")
+				.map(n => +n)
+				.reduce((sum, cur) => (sum += cur), 0)
+		)
+		.sort((a, b) => b - a)
+		.slice(0, 3)
+		.reduce((sum, cur) => (sum += cur), 0);
+
+	console.log(output);
+}
