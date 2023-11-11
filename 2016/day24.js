e26a9ddec3bca6401bcfcb1c71b05ffe1a955143
Taxato <taxatoftw@gmail.com>
2023-11-11 13:06:20 +0100

    day24

--- 2016/day24.js
+++ 2016/day24.js
@@ -1,4 +1,10 @@
-import { create2DArr, loop2DArr, timeUsed } from "../helper.js";
+import {
+	combinations,
+	create2DArr,
+	loop2DArr,
+	permutations,
+	timeUsed,
+} from "../helper.js";
 const startTime = Date.now();
 
 import { day24input as input } from "./inputs.js";
@@ -12,15 +18,27 @@ loop2DArr(map, (x, y) => {
 });
 pointsOfInterest = pointsOfInterest.sort((a, b) => a.num - b.num);
 
-function dstBetweenPoints(map, pointA, pointB) {
-	const queue = [pointA];
-	const visited = new Set([pointA]);
+function dstBetweenPoints(map, start, goal) {
+	const queue = [start];
+	const visited = new Set([hashPos(start)]);
 
 	while (queue.length) {
-		const pos = queue.shift();
+		const pos = { steps: 0, ...queue.shift() };
+
+		if (pos.x === goal.x && pos.y === goal.y) {
+			return pos.steps;
+		}
+
+		for (const neighbor of getNeighbors(map, pos)) {
+			if (visited.has(hashPos(neighbor))) continue;
+			else {
+				visited.add(hashPos(neighbor));
+				queue.push({ ...neighbor, steps: pos.steps + 1 });
+			}
+		}
 	}
 
-	function getNeighbors(map, x, y) {
+	function getNeighbors(map, { x, y }) {
 		const neighbors = [];
 		for (const dir of [
 			[0, 1],
@@ -32,19 +50,82 @@ function dstBetweenPoints(map, pointA, pointB) {
 			const newY = y + dir[1];
 
 			if (
-				newX < 0 ||
-				newX > map.length ||
-				newY < 0 ||
-				newY > map[0].length ||
-				map[x][y] === "#"
-			)
-				continue;
-
-			neighbors.push({ x: newX, y: newY });
+				newX >= 0 &&
+				newX < map.length &&
+				newY >= 0 &&
+				newY < map[0].length &&
+				map[newX][newY] !== "#"
+			) {
+				neighbors.push({ x: newX, y: newY });
+			} else continue;
 		}
 		return neighbors;
 	}
+
+	function hashPos(pos) {
+		return `${pos.x}/${pos.y}`;
+	}
 }
+
+function shortestPath(map, pointsOfInterest) {
+	const allDistances = {};
+
+	for (const pair of combinations(pointsOfInterest, 2)) {
+		const [a, b] = pair;
+
+		const dst = dstBetweenPoints(map, a, b);
+
+		const key = `${a.num}/${b.num}`;
+		const reverseKey = `${b.num}/${a.num}`;
+		allDistances[key] = dst;
+		allDistances[reverseKey] = dst;
+	}
+
+	let shortestDst = Infinity;
+
+	for (const perm of permutations(pointsOfInterest.slice(1))) {
+		const points = [pointsOfInterest[0], ...perm];
+
+		let totalDst = 0;
+		for (let i = 0; i < perm.length; i++) {
+			totalDst += allDistances[`${points[i].num}/${points[i + 1].num}`];
+		}
+		shortestDst = Math.min(shortestDst, totalDst);
+	}
+	return shortestDst;
+}
+
+function shortestPathReturnToStart(map, pointsOfInterest) {
+	const allDistances = {};
+
+	for (const pair of combinations(pointsOfInterest, 2)) {
+		const [a, b] = pair;
+
+		const dst = dstBetweenPoints(map, a, b);
+
+		const key = `${a.num}/${b.num}`;
+		const reverseKey = `${b.num}/${a.num}`;
+		allDistances[key] = dst;
+		allDistances[reverseKey] = dst;
+	}
+
+	let shortestDst = Infinity;
+
+	for (const perm of permutations(pointsOfInterest.slice(1))) {
+		const points = [pointsOfInterest[0], ...perm];
+
+		let totalDst = 0;
+		for (let i = 0; i < perm.length; i++) {
+			totalDst += allDistances[`${points[i].num}/${points[i + 1].num}`];
+		}
+		totalDst += allDistances[`${points.at(-1).num}/0`];
+		shortestDst = Math.min(shortestDst, totalDst);
+	}
+	return shortestDst;
+}
+
+console.log("Part one:", shortestPath(map, pointsOfInterest));
+console.log("Part two:", shortestPathReturnToStart(map, pointsOfInterest));
 
 const endTime = Date.now();
 timeUsed(startTime, endTime);
