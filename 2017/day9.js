a342a5ffc8f429c9f87ff192679020b042cc7571
Taxato <taxatoftw@gmail.com>
2023-11-14 15:04:48 +0100

    day9wip

---
+++ 2017/day9.js
@@ -0,0 +1,7 @@
+import { timeUsed } from "../helper.js";
+const startTime = Date.now();
+
+import { day9inputTrimmed as input } from "./inputs.js";
+
+const endTime = Date.now();
+timeUsed(startTime, endTime);
