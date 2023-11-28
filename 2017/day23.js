5319a73d2712aed0b524c2a98e7099a178990e74
Taxato <taxatoftw@gmail.com>
2023-11-28 13:19:58 +0100

    Update day23.js

--- 2017/day23.js
+++ 2017/day23.js
@@ -45,37 +45,7 @@ while (true) {
 	if (lineIndex < 0 || lineIndex >= input.length) break;
 }
 
-function partTwo() {
-	let r = {
-		b: 67,
-		c: 67,
-		d: 0,
-		f: 0,
-		g: 0,
-		h: 0,
-	};
-	r.b = r.b * 100 + 100000;
-	r.c = r.b + 17000;
-	do {
-		r.f = 1;
-		r.d = 2;
-		for (let d = r.d; d * d < r.b; ++d) {
-			if (r.b % d === 0) {
-				r.f = 0;
-				break;
-			}
-		}
-		if (r.f === 0) r.h++;
-		r.g = r.b - r.c;
-		r.b += 17;
-	} while (r.g !== 0);
-
-	return r.h;
-}
-
 console.log("Part one:", numMul);
-
-console.log("Part two:", partTwo());
 
 const endTime = Date.now();
 timeUsed(startTime, endTime);
