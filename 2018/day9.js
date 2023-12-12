a356108aff6e8e0cc64bc5f7842343d60df2461e
Taxato <taxatoftw@gmail.com>
2023-12-12 14:47:34 +0100

    day9wip

---
+++ 2018/day9.js
@@ -0,0 +1,14 @@
+import { time } from "../helper.js";
+const startTime = process.hrtime();
+
+function marbleMania(numPlayers, lastMarbleWorth) {
+	let turn = 1;
+	let index = 0;
+	let marbles = [0];
+}
+
+function marbleTurn(turn,index,marbles) {
+    if()
+}
+
+time(startTime);
