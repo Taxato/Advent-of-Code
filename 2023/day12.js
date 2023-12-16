ca66b48f92a864f8dddfda76d393e743dc99d710
Taxato <taxatoftw@gmail.com>
2023-12-16 13:02:03 +0100

    day12

--- 2023/day12.js
+++ 2023/day12.js
@@ -1,62 +1,77 @@
 import { readFileSync } from "fs";
-import { sumArr, time } from "../helper.js";
+import { time } from "../helper.js";
 const startTime = process.hrtime();
 
 const input = readFileSync("./day12input.txt", { encoding: "utf8" });
-// const input = readFileSync("./day12test.txt", { encoding: "utf8" });
+const test = readFileSync("./day12test.txt", { encoding: "utf8" });
 
-class Row {
-	constructor(springs, numbers) {
-		this.springs = springs;
-		this.numbers = numbers;
-		this.groups = [];
-	}
+const cache = {};
 
-	get numArrangements() {
-		let num = 0;
+function solve(input, partTwo = false) {
+	let total = 0;
 
-		const unknownIdcs = [];
-		for (let i = 0; i < this.springs.length; i++)
-			if (this.springs.charAt(i) === "?") unknownIdcs.push(i);
+	for (const row of input.split("\n")) {
+		let [springs, counts] = row.split(" ");
 
-		const bin = "1".repeat(unknownIdcs.length);
-
-		for (let i = 0; i <= Number("0b" + bin); i++) {
-			const combo = i
-				.toString(2)
-				.padStart(bin.length, "0")
-				.split("")
-				.map(Number);
-
-			const strArr = this.springs.split("");
-			for (let j = 0; j < combo.length; j++) {
-				strArr[unknownIdcs[j]] = combo[j] === 1 ? "#" : ".";
-			}
-			const newSprings = strArr.join("");
-
-			if (this.isValid(newSprings)) num++;
+		if (partTwo) {
+			springs = (springs + "?").repeat(5).slice(0, -1);
+			counts = (counts + ",").repeat(5).slice(0, -1);
 		}
 
-		return num;
+		counts = counts.split(",").map(Number);
+		total += numSolutions(springs, counts);
 	}
 
-	isValid(springs) {
-		if (springs.match(/#/g)?.length !== sumArr(this.numbers)) return false;
-
-		const regex = new RegExp(
-			`${this.numbers.map(n => "#".repeat(n)).join(".+")}`
-		);
-		return regex.test(springs);
-	}
+	return total;
 }
 
-let sum = 0;
-input.split("\n").forEach(line => {
-	let [springs, numbers] = line.split(" ");
-	numbers = numbers.split(",").map(Number);
-	const newRow = new Row(springs, numbers);
-	sum += newRow.numArrangements;
-});
-console.log(sum);
+function numSolutions(springs, counts) {
+	// console.log(springs, counts);
+
+	const key = springs + "||" + counts.join("/");
+	if (key in cache) return cache[key];
+
+	let res;
+
+	if (springs.length === 0) {
+		res = counts.length === 0 ? 1 : 0;
+		cache[key] = res;
+		return res;
+	}
+	if (counts.length === 0) {
+		res = /#/.test(springs) ? 0 : 1;
+		cache[key] = res;
+		return res;
+	}
+
+	let [char, ...remSprings] = springs.split("");
+	remSprings = remSprings.join("");
+	let remCounts = [...counts];
+
+	if (char === ".") res = numSolutions(remSprings, remCounts);
+	else if (char === "#") {
+		const count = remCounts[0];
+		if (
+			springs.length >= count &&
+			!/\./.test(springs.substring(0, count)) &&
+			(springs.length === count || springs.charAt(count) !== "#")
+		) {
+			// console.log(springs, remCounts);
+			remSprings = remSprings.slice(count);
+			remCounts = remCounts.slice(1);
+			res = numSolutions(remSprings, remCounts);
+		} else res = 0;
+	} else if (char === "?") {
+		res =
+			numSolutions("." + remSprings, remCounts) +
+			numSolutions("#" + remSprings, remCounts);
+	}
+
+	cache[key] = res;
+	return res;
+}
+
+console.log("Part one:", solve(input));
+console.log("Part two:", solve(input, true));
 
 time(startTime);
